cb21aaaf1b9ae6235582b4fcb42b82245ffb29cc
Anne-todao <annetodao@gmail.com>
2026-04-23 14:11:27 -0300

    commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import Cardapio from './components/Cardapio'
+
+function App() {
+
+  return (
+    <>
+      <h1>cozinha </h1>
+      <Cardapio />
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Cardapio.jsx
@@ -0,0 +1,50 @@
+// imports que o professor vai passar
+import styles from './Cardapio.module.css'
+import { useState, useEffect } from 'react'
+
+function Cardapio() {
+
+
+  //useState que o professor vai passar
+  const [pratos, setPratos] = useState([]);
+  
+  // useEffect que o professor vai passar par abuscar os pratos e o estado de
+useEffect(() => {
+  fetch('https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood')
+    .then(response => response.json())
+    .then(data => setPratos(data.meals))
+    .catch(error => console.error('Erro ao buscar os pratos:', error));
+}, [])
+
+  // carregamento que o professor vai passar
+  const [carregando, setCarregando] = useState(true);
+  useEffect(() => {
+    fetch('https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood')
+      .then(response => response.json())
+      .then(data => {
+        setPratos(data.meals);
+        setCarregando(false);
+      })
+      .catch(error => {
+        console.error('Erro ao buscar os pratos:', error);
+        setCarregando(false);
+      });
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <h1>Cardápio de Frutos do Mar</h1>
+      {carregando && <p className={styles.loading}>Carregando...</p>}
+      <div className={styles.grid}>
+        {pratos.map(item => (
+          <div key={item.idMeal} className={styles.card}>
+            <img src={item.strMealThumb} alt={item.strMeal} />
+            <h3>{item.strMeal}</h3>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Cardapio
